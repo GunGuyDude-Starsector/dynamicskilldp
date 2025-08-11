3a53dfcca88f20800d88451e86ea62bc4b72fb7f
GunGuyDude <147735971+GunGuyDude@users.noreply.github.com>
2025-08-11 13:58:01 +0800

    Actually works now (kind of, hardcoded some values)

--- data/scripts/DynamicSkillDP.java
+++ data/scripts/DynamicSkillDP.java
@@ -1,4 +1,4 @@
-package dynamicskilldp;
+package data.scripts;
 
 import com.fs.starfarer.api.BaseModPlugin;
 import com.fs.starfarer.api.Global;
@@ -7,20 +7,22 @@ import com.fs.starfarer.api.impl.campaign.skills.BaseSkillEffectDescription;
 public class DynamicSkillDP extends BaseModPlugin {
 
     public static float BASE_VALUE = 240f;
+    public static float BASE_MAX_BATTLE_SIZE = 400f;
 
-    public static float FIGHTER_BAYS_THRESHOLD = (float) Math.floor(BASE_VALUE / 30);
+    public static float FIGHTER_BAYS_THRESHOLD = (float) Math.floor(BASE_VALUE / 30f);
     public static float OP_THRESHOLD = (float) Math.floor(BASE_VALUE);
-	public static float OP_LOW_THRESHOLD = (float) Math.floor(BASE_VALUE / 2);
-	public static float OP_ALL_LOW_THRESHOLD = (float) Math.floor(BASE_VALUE / 2);
+	public static float OP_LOW_THRESHOLD = (float) Math.floor(BASE_VALUE / 2f);
+	public static float OP_ALL_LOW_THRESHOLD = (float) Math.floor(BASE_VALUE / 2f);
 	public static float OP_ALL_THRESHOLD = (float) Math.floor(BASE_VALUE);
-	public static float PHASE_OP_THRESHOLD = (float) Math.floor(BASE_VALUE / 6);
-	public static float MILITARIZED_OP_THRESHOLD = (float) Math.floor(BASE_VALUE / 48);
-	public static float AUTOMATED_POINTS_THRESHOLD = (float) Math.floor(BASE_VALUE / 2);
+	public static float PHASE_OP_THRESHOLD = (float) Math.floor(BASE_VALUE / 6f);
+	public static float MILITARIZED_OP_THRESHOLD = (float) Math.floor(BASE_VALUE / 48f);
+	public static float AUTOMATED_POINTS_THRESHOLD = (float) Math.floor(BASE_VALUE / 2f);
 
     @Override
-    public void onGameLoad() {
-        int battleSize = Global.getSettings.getBattleSize();
-        float multiplier = battleSize / BASE_VALUE;
+    public void onApplicationLoad() {
+        //int battleSize = Global.getSettings.getBattleSize();
+        float battleSize = 800f;
+        float multiplier = battleSize / BASE_MAX_BATTLE_SIZE;
 
         BaseSkillEffectDescription.FIGHTER_BAYS_THRESHOLD = FIGHTER_BAYS_THRESHOLD * multiplier;
         BaseSkillEffectDescription.OP_THRESHOLD = OP_THRESHOLD * multiplier;
